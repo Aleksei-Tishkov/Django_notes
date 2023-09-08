8b7d56b3aa877575f92b1d4dc3d81cd1643e173a
Aleksei Tishkov <tscenic.ua@gmail.com>
2023-09-08 15:39:01 +0530

    Basic home page added

--- Django_notes/settings.py
+++ Django_notes/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'add_note',
+    'home',
+    'login',
+    'logout',
+    'notes',
+    'reg'
 ]
 
 MIDDLEWARE = [
